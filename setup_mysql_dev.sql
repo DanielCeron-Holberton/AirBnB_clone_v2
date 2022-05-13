01801a33c76c6bfc0d5c60e77337c432a179531b
Daniel <3403@holbertonschool.com>
2022-05-13 18:42:36 -0500

    [ADD] - Documentation based on functionality

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+-- Creates a new DATABASE and new user related to database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
